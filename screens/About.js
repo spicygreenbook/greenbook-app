0ee0029bd366df4eb852010bcbc6a7de1eabb096
Dan Williams <pleaseshutup@gmail.com>
2020-09-27 17:46:21 +0000

    added about screen with dynamic content from prismic

--- screens/About.js
+++ screens/About.js
@@ -2,12 +2,50 @@ import React from 'react';
 import { useStateValue } from "../components/State";
 import {View, Text, StyleSheet, Button, Platform} from 'react-native';
 import { Link } from "../components/Link"; 
+import { RichText } from "../components/RichText"; 
+import Head from "next/head";
+import { getStyles, Theme } from '../utils';
+
 
 function Page(props) {
-    return <View style={styles.container} style={{marginTop: 100}}>
-        <Text>This is the aabout screen</Text>
-        <Link href="/" title="Home Screen" />
-    </View>
+
+    const [{ view, isWeb, dimensions }, dispatch] = useStateValue();
+    const styles = StyleSheet.create(getStyles('text_header2, section, content', {isWeb}));
+    console.log('page props', props)
+    const content = props.content || {}
+
+    return (
+    <React.Fragment>
+        <Head>
+            <title>{content.page_title} - Spicy Green Book</title>
+            {content.description && 
+                <meta
+                name="description"
+                content={content.description}
+                key="description"
+                />
+            }
+            {content.description && 
+                <meta
+                name="og:description"
+                content={content.description}
+                key="og:description"
+                />
+            }
+            <meta property="og:title" content={content.page_title + " - Spicy Green Book"} key="title" />
+            <meta property="og:url" content={"https://spicygreenbook.com/" + content.uid } key="og:url" />
+        </Head>
+        <View style={[styles.section, {backgroundColor: Theme.green_bg, paddingTop: 180}]}>
+            <View style={[styles.content, {flexDirection: 'column', alignItems: 'center'}]}>
+                <Text accessibilityRole="header" aria-level="2" style={[styles.text_header2, {color: '#fff'}]}>{content.page_title}</Text>
+            </View>
+        </View>
+        <View style={[styles.section]}>
+            <View style={styles.content}>
+                <RichText render={content._body} isWeb={isWeb} />
+            </View>
+        </View>
+    </React.Fragment>)
 }
 
 const styles = StyleSheet.create({
