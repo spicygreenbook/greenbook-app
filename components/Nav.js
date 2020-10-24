509967c7212fdc099d60a98b0e27057008357561
Dan Williams <pleaseshutup@gmail.com>
2020-10-24 13:10:16 +0000

    fade animation on nav going up and down, border on light theme nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, Easing, Animated, Button, Image, TouchableOpacity } from 'react-native';
 //import {TouchableOpacity} from 'react-native-gesture-handler';
 import { getStyles, Theme } from '../utils';
-import {useStateValue} from "../components/State";
+import { useStateValue } from "../components/State";
 import { AntDesign } from '@expo/vector-icons'; 
 import { Link, Click } from '../components/Link';
 
@@ -13,8 +13,50 @@ export default function(props) {
 
     const [ active, setActive ] = useState('');
 
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    const ColorIn = () => {
+        // Will change fadeAnim value to 1 in 5 seconds
+        Animated.timing(fadeAnim, {
+            easing: Easing.bounce,
+            toValue: 0,
+            duration: 500,
+        }).start();
+    };
+
+    const ColorOut = () => {
+        // Will change fadeAnim value to 0 in 5 seconds
+        Animated.timing(fadeAnim, {
+            easing: Easing.ease,
+            toValue: 1,
+            duration: 500,
+        }).start();
+    };
+
+    useEffect(() => {
+        if (props.isScrolled) {
+            ColorIn();
+        } else {
+            ColorOut()
+        }
+        console.log('animation effect hook')
+    }, [props.isScrolled])
+
+    let bg_dark = fadeAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['#000', 'transparent']
+    });
+
     return (
-        <View style={[styles.nav, props.isScrolled ? {backgroundColor: props.theme == 'light' ? '#fff' : '#000'} : {backgroundColor: props.theme == 'light' ? '#fff' : 'transparent'}]}>
+        <Animated.View
+        style={[styles.fadingContainer,
+          styles.nav,
+          {
+            borderBottomWidth: props.theme === 'light' ? 2 : 0,
+            borderColor: Theme.green,
+            backgroundColor: props.theme == 'light' ? '#fff' : bg_dark
+            }
+        ]}>
             <View style={{padding: 20, flex: 1, alignContent: 'center', borderRightWidth: 2, borderColor: props.theme == 'light' ? Theme.green : '#fff'}}>
                 <Link href="/" fill>
                     <View style={{height: '100%'}}>
@@ -90,7 +132,7 @@ export default function(props) {
                     </React.Fragment>
                 )}
             </View>
-        </View>
+        </Animated.View>
     )
 
 }
