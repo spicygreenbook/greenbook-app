1ed52609409ed29a15b6089c7037be5f61cfca0e
Arax1 <anandraju55@hotmail.com>
2020-11-05 13:10:33 -0500

    autoformatted the code and adjusted scrollToIndex parameter

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useStateValue } from "../components/State";
 import { getStyles, Theme } from '../utils';
 import { Animated, View, Text, Image, StyleSheet, FlatList, ImageBackground, TouchableOpacity } from 'react-native';
-import { Entypo } from '@expo/vector-icons'; 
-import { EvilIcons } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
 import { responsiveImageWidthCDN } from '../utils';
 
 let currentIndexImage = 0;
@@ -16,7 +16,7 @@ const viewableItemsChangedConfigImage = {
     itemVisiblePercentThreshold: 50
 };
 
-export default function(props) {
+export default function (props) {
 
     const { images, firstIndex } = props;
 
@@ -30,7 +30,7 @@ export default function(props) {
     let imageRefBottom;
     const scrollToIndexImage = (obj, len) => {
         if (obj.index < 0) { obj.index = 0; }
-        if (obj.index > len-1) { obj.index = len-1; }
+        if (obj.index > len - 1) { obj.index = len - 1; }
         if (imageRefTop) {
             imageRefTop.scrollToIndex(obj)
         } else {
@@ -58,13 +58,13 @@ export default function(props) {
     useEffect(() => {
         setTimeout(() => {
             if (firstIndex) {
-                scrollToIndexImage({animated: true, index: firstIndex}, images.length)
+                scrollToIndexImage({ animated: true, index: firstIndex }, images.length)
             }
         }, 10)
     }, [])
 
-    return <View style={{position: 'absolute', left: 0, top:0, bottom:0, right:0, backgroundColor: '#000', zIndex: 6}}>
-        <View style={{position: 'absolute', top: 0, left: 0, right:0, height: '80%'}}>
+    return <View style={{ position: 'absolute', left: 0, top: 0, bottom: 0, right: 0, backgroundColor: '#000', zIndex: 6 }}>
+        <View style={{ position: 'absolute', top: 0, left: 0, right: 0, height: '80%' }}>
             <FlatList
                 horizontal
                 showsHorizontalScrollIndicator={false}
@@ -73,12 +73,12 @@ export default function(props) {
                 onViewableItemsChanged={viewableItemsChangedImage}
                 viewabilityConfig={viewableItemsChangedConfigImage}
                 renderItem={({ item, index, separators }) => (
-                    <ImageBackground source={{uri: item.url + '&w=' + responsiveImageWidthCDN({containerWidth: config.top.width})}} style={{flex:1, width: config.top.width, height: '100%'}} resizeMode={'contain'} />
+                    <ImageBackground source={{ uri: item.url + '&w=' + responsiveImageWidthCDN({ containerWidth: config.top.width }) }} style={{ flex: 1, width: config.top.width, height: '100%' }} resizeMode={'contain'} />
                 )}
                 keyExtractor={(item, index) => 'image' + index}
             />
         </View>
-        <View style={{position: 'absolute', bottom: 0, left: 0, right:0, height: '20%'}}>
+        <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: '20%' }}>
             <FlatList
                 horizontal
                 showsHorizontalScrollIndicator={false}
@@ -89,33 +89,33 @@ export default function(props) {
                 renderItem={({ item, index, separators }) => (
                     <View>
                         <TouchableOpacity onPress={e => {
-                            scrollToIndexImage({animated: true, index: index}, images.length)
+                            scrollToIndexImage({ animated: true, index: index }, images.length)
                         }}>
-                            <ImageBackground source={{uri: item.url + '&w=400'}} style={{width: config.bottom.width, height: config.bottom.height}}>
+                            <ImageBackground source={{ uri: item.url + '&w=400' }} style={{ width: config.bottom.width, height: config.bottom.height }}>
                             </ImageBackground>
                         </TouchableOpacity>
                     </View>
                 )}
                 keyExtractor={(item, index) => 'image' + index}
             />
-            <View style={{position: 'absolute', top: '50%', marginTop: -24, left:10, right:10, height: 0, flex: 1}}>
-                <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
-                    <View style={{width:48, height:48}}>
-                        <TouchableOpacity onPress={(e) => scrollToIndexImage({animated: true, index: currentIndexImage-1}, 10)}>
+            <View style={{ position: 'absolute', top: '50%', marginTop: -24, left: 10, right: 10, height: 0, flex: 1 }}>
+                <View style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+                    <View style={{ width: 48, height: 48 }}>
+                        <TouchableOpacity onPress={(e) => scrollToIndexImage({ animated: true, index: currentIndexImage - 1 }, images.length)}>
                             <Entypo name="chevron-thin-left" size={48} color="#fff" />
                         </TouchableOpacity>
                     </View>
-                    <View style={{width:48, height:48, alignItems: 'flex-end'}}>
-                        <TouchableOpacity onPress={(e) => scrollToIndexImage({animated: true, index: currentIndexImage+1}, 10)}>
+                    <View style={{ width: 48, height: 48, alignItems: 'flex-end' }}>
+                        <TouchableOpacity onPress={(e) => scrollToIndexImage({ animated: true, index: currentIndexImage + 1 }, images.length)}>
                             <Entypo name="chevron-thin-right" size={48} color="#fff" />
                         </TouchableOpacity>
                     </View>
                 </View>
             </View>
         </View>
-        <View style={{position: 'absolute', top: isWeb ? 10 : 80, right: 10}}>
+        <View style={{ position: 'absolute', top: isWeb ? 10 : 80, right: 10 }}>
             <TouchableOpacity onPress={e => {
-                dispatch({type: 'lightbox', value: false})
+                dispatch({ type: 'lightbox', value: false })
             }}>
                 <EvilIcons name="close" size={48} color="#fff" />
             </TouchableOpacity>
