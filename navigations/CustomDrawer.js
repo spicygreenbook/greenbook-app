1efc221773b5ef0fd08463edca27a67dab45de7e
Marv <mrvncaragay@gmail.com>
2020-11-14 13:52:37 -0800

    Add url links to icon

--- navigations/CustomDrawer.js
+++ navigations/CustomDrawer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Linking } from 'react-native';
 import { Container, Item, Header, Footer, Content, FooterTab, Button, Icon } from 'native-base';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { Theme } from '../utils';
@@ -14,19 +14,19 @@ const CustomDrawer = (props) => {
       </Content>
       <Footer style={{ backgroundColor: Theme.green }}> 
         <FooterTab>
-          <Button>
+          <Button onPress={() => Linking.openURL("https://instagram.com/spicygreenbook")} >
             <Icon type="Fontisto" style={styles.default} name="instagram" size={22} />
           </Button>
 
-          <Button>
+          <Button onPress={() => Linking.openURL("https://twitter.com/spicygreenbook")}>
             <Icon type="Fontisto" style={styles.default} name="twitter" size={22} />
           </Button>
 
-          <Button>
+          <Button onPress={() => Linking.openURL("https://www.linkedin.com/company/spicy-green-book/")}>
             <Icon type="Fontisto" style={styles.default} name="linkedin" size={22} />
           </Button>
 
-          <Button>
+          <Button onPress={() => Linking.openURL("https://www.facebook.com/SpicyGreenBook/")}>
             <Icon type="Fontisto" style={styles.default} name="facebook" size={22} />
           </Button>
         </FooterTab>
