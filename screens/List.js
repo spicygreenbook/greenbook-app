136fa3f604ba5caba5d5a08ddaf9bb9f1d1012f6
Marv <mrvncaragay@gmail.com>
2020-11-30 13:45:56 -0800

    Fix network request failed for native

--- screens/List.js
+++ screens/List.js
@@ -259,7 +259,7 @@ function Page(props) {
         () => {
             setSearch(fixSearch(query.q));
             setProcessedSearchTerms(searchSeries(fixSearch(query)));
-            if (location) {
+            if (isWeb && location) {
                 fetch('/api/geocode?query=' + location).then(res => res.json()).then(json => {
                     if (json.coords) {
                         setGeoLocation(json.coords);
