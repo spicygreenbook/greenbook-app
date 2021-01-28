39c8b96d43495b2c5d2adb9e72639af864d20619
Arax1 <anandraju55@hotmail.com>
2021-01-28 16:24:34 -0500

    font and styling changes made to text

--- navigations/Onboarding.js
+++ navigations/Onboarding.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, SafeAreaView, Text, Dimensions, ScrollView, StatusBar, StyleSheet, PixelRatio, Image } from 'react-native';
+import { View, SafeAreaView, Text, Dimensions, ScrollView, StatusBar, StyleSheet, Image } from 'react-native';
 
 const Onboarding = () => {
 
@@ -58,16 +58,18 @@ const Onboarding = () => {
         header_white: {
             position: 'absolute',
             textAlign: 'center',
-            fontSize: 25,
+            fontSize: 30,
             fontWeight: 'bold',
+            fontFamily: 'KnockoutWelterWeight',
             color: 'white',
-            bottom: 200
+            bottom: 200,
 
         },
 
         paragraph_white: {
             position: 'absolute',
             fontSize: 14,
+            fontFamily: 'ApercuMedium',
             color: 'white',
             textAlign: 'center',
             bottom: 100,
@@ -76,8 +78,9 @@ const Onboarding = () => {
 
         header_black: {
             position: 'absolute',
+            fontFamily: 'KnockoutWelterWeight',
             textAlign: 'center',
-            fontSize: 25,
+            fontSize: 30,
             fontWeight: 'bold',
             color: 'black',
             top: 455
@@ -89,6 +92,7 @@ const Onboarding = () => {
             position: 'relative',
             textAlign: 'center',
             fontSize: 14,
+            fontFamily: 'ApercuMedium',
             color: 'black',
             top: 480,
             padding: 20,
@@ -135,7 +139,7 @@ const Onboarding = () => {
                         <View style={styles.wrapper}>
                             <Image source={require('../public/images/Screen1.png')} style={styles.imageStyle1} />
                             <Image source={require('../public/images/nav_resized_2.png')} style={styles.logo} />
-                            <Text style={styles.header_white}>Discover and support over 150 local Black-owned Businesses</Text>
+                            <Text style={styles.header_white}>Discover and support over 150 local {"\n"} Black-owned Businesses</Text>
                             <Text style={styles.paragraph_white}>Spicy Green Book is a non-profit focused on helping black owned businessses succeed through
                             a netowrk of volunteer professionals
                             </Text>
@@ -145,7 +149,7 @@ const Onboarding = () => {
                         <View style={styles.wrapper}>
                             <Image source={require('../public/images/nav_resized_3.png')} style={styles.logo2} />
                             <Image source={require('../public/images/Screen2.png')} style={styles.imageStyle2} />
-                            <Text style={styles.header_black}>Search for businessses that are located near you.</Text>
+                            <Text style={styles.header_black}>Search for businessses that are {"\n"} located near you.</Text>
                             <Text style={styles.paragraph_black}>Tap on the Browse tab to discover and search for a specific
                             businesss. We are continuously adding more businesss to SGB.
                             </Text>
@@ -155,7 +159,7 @@ const Onboarding = () => {
                         <View style={styles.wrapper}>
                             <Image source={require('../public/images/nav_resized_3.png')} style={styles.logo2} />
                             <Image source={require('../public/images/Screen3.png')} style={styles.imageStyle2} />
-                            <Text style={styles.header_black}>Learn about each businesss and their story</Text>
+                            <Text style={styles.header_black}>Learn about each businesss and {"\n"} their story</Text>
                             <Text style={styles.paragraph_black}>Choose a business to learn about what they do, how to get in contact,
                             and how to support them.
                             </Text>
