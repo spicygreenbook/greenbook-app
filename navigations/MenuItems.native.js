3a08955bfb32a769e302f7310a440ca9c114c414
Liron de Castro <60845154+lirondco@users.noreply.github.com>
2021-05-06 16:53:20 -0700

    added social links to the mobile app's drawer

--- navigations/MenuItems.native.js
+++ navigations/MenuItems.native.js
@@ -2,9 +2,56 @@ import React from "react";
 import { Theme } from "../utils";
 import Collapsible from "react-native-collapsible";
 import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, Linking, TouchableOpacity, StyleSheet } from "react-native";
 import { FontAwesome } from '@expo/vector-icons'; 
+import { FooterTab, Footer, Button, Icon } from 'native-base';
 
+const SocialLinks = () => {
+  const styles = StyleSheet.create({
+    default: { color: Theme.green, fontSize: 22, }
+    });
+  return (
+    <View>
+      <Footer style={{ backgroundColor: "white",borderBottomWidth: 2,
+        borderBottomColor: Theme.green }}> 
+        <FooterTab>
+          <Button onPress={() => Linking.openURL("https://instagram.com/spicygreenbook")} >
+            <Icon type="FontAwesome" style={styles.default} name="instagram" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("https://twitter.com/spicygreenbook")}>
+            <Icon type="FontAwesome" style={styles.default} name="twitter" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("https://www.linkedin.com/company/spicy-green-book/")}>
+            <Icon type="FontAwesome" style={styles.default} name="linkedin" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("https://www.facebook.com/SpicyGreenBook/")}>
+            <Icon type="FontAwesome" style={styles.default} name="facebook" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("https://www.youtube.com/channel/UCS5gEWNUF2fibLwwxFibKGg")}>
+            <Icon type="FontAwesome" style={styles.default} name="youtube" size={22} />
+          </Button>
+        </FooterTab>
+      </Footer>
+      <Footer style={{ backgroundColor: "white", backgroundColor: "white",borderBottomWidth: 2,
+        borderBottomColor: Theme.green }}>
+        <FooterTab>
+          <Button onPress={() => Linking.openURL("https://github.com/spicygreenbook/greenbook-app")}>
+            <Icon type="FontAwesome" style={styles.default} name="github" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("https://www.behance.net/spicygreenbook/")}>
+            <Icon type="FontAwesome" style={styles.default} name="behance" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("https://www.pinterest.com/spicygreenbook/")}>
+            <Icon type="FontAwesome" style={styles.default} name="pinterest" size={22} />
+          </Button>
+          <Button onPress={() => Linking.openURL("mailto:admin@spicygreenbook.org")}>
+            <Icon type="FontAwesome" style={styles.default} name="envelope" size={22} />
+          </Button>
+        </FooterTab>
+      </Footer>
+    </View>
+  );
+}
 
 export function RegularItem(props) {
   return (
@@ -123,7 +170,7 @@ export function CustomDrawerContent(props) {
       <RegularItem label="Store" link="Store" navigation={props.navigation} />
       <RegularItem label="FAQ" link="FAQ" navigation={props.navigation} />
       <RegularItem label="Contact" link="Contact" navigation={props.navigation} />
-
+      <SocialLinks />
     </DrawerContentScrollView>
   );
 }
