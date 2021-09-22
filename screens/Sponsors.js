d4e3450e09f8d72cb2de8f60d220351e4f8935d8
danilobatson <djbatson19@gmail.com>
2021-09-21 18:54:43 -0700

    501 sponsors page

--- screens/Sponsors.js
+++ screens/Sponsors.js
@@ -288,7 +288,8 @@ function Page(props) {
 												our growth. Together, we can empower more Black-owned
 												businesses, diversify the world’s business landscape,
 												and bring amazing food, culture, and experiences to all!
-												Spicy Green Book is a <b>501(c)(3) nonprofit organization,</b> EIN 85-1876971.
+												<br/>
+												Spicy Green Book is a 501(c)(3) nonprofit organization, EIN 85-1876971.
 											</Text>
 										</View>
 									</View>
